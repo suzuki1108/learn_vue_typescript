cd0890419318b3c130bf4eb04b61d818d31f226a
suzuki1108 <suzukinoko.1108@icloud.com>
2022-01-29 13:39:11 +0900

    テストコード含む雛形の修正

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,15 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+  <div>
+    <h1>ホーム</h1>
   </div>
 </template>
 
 <script lang="ts">
-import Vue from 'vue';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+import { defineComponent } from '@vue/composition-api';
 
-export default Vue.extend({
-  name: 'Home',
-  components: {
-    HelloWorld,
+export default defineComponent({
+  setup() {
+    //
   },
 });
 </script>
